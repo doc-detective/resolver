9b5a822fb8fd7e2b29e3c9b4382da4dda79be44e
hawkeyexl <hawkeyexl@gmail.com>
2025-05-22 09:56:14 -0700

    Replace newline character with OS-specific line ending in parseContent function

--- dev/index.js
+++ dev/index.js
@@ -7,15 +7,54 @@ main();
 
 async function main() {
   const json = {
-    input: "/home/hawkeyexl/Workspaces/resolver/dev/dev.spec.json",
-    integrations:{
-      openApi: [
-        {
-          name: "reqres",
-          descriptionPath: "/home/hawkeyexl/Workspaces/resolver/dev/reqres.openapi.json",
-        }
-      ]
-    },
+    input: "/home/hawkeyexl/Workspaces/resolver/dev/doc-content copy.md",
+    fileTypes: [
+      {
+        name: "markdown",
+        extensions: ["md", "markdown", "mdx"],
+        inlineStatements: {
+          testStart: [
+            "{\\/\\*\\s*test\\s+?([\\s\\S]*?)\\s*\\*\\/}",
+            "<!--\\s*test\\s*([\\s\\S]*?)\\s*-->",
+            "\\[comment\\]:\\s+#\\s+\\(test\\s*(.*?)\\s*\\)",
+            "\\[comment\\]:\\s+#\\s+\\(test start\\s*(.*?)\\s*\\)",
+          ],
+          testEnd: [
+            "{\\/\\*\\s*test end\\s*\\*\\/}",
+            "<!--\\s*test end\\s*([\\s\\S]*?)\\s*-->",
+            "\\[comment\\]:\\s+#\\s+\\(test end\\)",
+          ],
+          ignoreStart: [
+            "{\\/\\*\\s*test ignore start\\s*\\*\\/}",
+            "<!--\\s*test ignore start\\s*-->",
+          ],
+          ignoreEnd: [
+            "{\\/\\*\\s*test ignore end\\s*\\*\\/}",
+            "<!--\\s*test ignore end\\s*-->",
+          ],
+          step: [
+            "{\\/\\*\\s*step\\s+?([\\s\\S]*?)\\s*\\*\\/}",
+            "<!--\\s*step\\s*([\\s\\S]*?)\\s*-->",
+            "\\[comment\\]:\\s+#\\s+\\(step\\s*(.*?)\\s*\\)",
+          ],
+        },
+        markup: [
+          {
+            name: "runPython",
+            regex: ["```(?:python)\\b\\s*\\n(?<code>.*?)(?=\\n```)"],
+            batchMatches: true,
+            actions: [
+              {
+                runCode: {
+                  language: "python",
+                  code: "$1",
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
     logLevel: "debug",
   };
   result = await detectAndResolveTests({ config: json });
